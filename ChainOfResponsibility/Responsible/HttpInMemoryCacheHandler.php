d9e185971af40e8c260dae2c6bcc31669dfa395b
张海生 <zhaisheng@rolaner.com>
2017-04-10 18:44:21 +0800

    模仿 chainofresponbility

---
+++ ChainOfResponsibility/Responsible/HttpInMemoryCacheHandler.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: work
+ * Date: 2017/4/10
+ * Time: 18:32
+ */
+namespace Pattern\ChainOfResponsibility\Responsible;
+use Pattern\ChainOfResponsibility\Handler;
+use Psr\Http\Message\RequestInterface;
+
+class HttpInMemoryCacheHandler extends Handler
+{
+    private $data;
+
+    public function __construct(array $data ,Handler $successor = null){
+        parent::__construct($successor);
+        $this->data = $data;
+    }
+
+    protected function processing(RequestInterface $request){
+        $key = sprintf(
+            '%s?%s',
+            $request->getUri()->getPath(),
+            $request->getUri()->getQuery()
+        );
+
+        if($request->getMethod() == 'GET' && isset($this->data[$key])){
+            return $this->data[$key];
+        }
+
+        return null;
+    }
+}
